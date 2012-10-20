c06169622d704f8f479d3f46b06ee34d795ac7c1
Aaron VonderHaar <aaron@buildsomethingawesome.org>
2012-10-20 11:07:57 -0700

    Draw the game graphics and give the window a size.

--- src/PongGame.java
+++ src/PongGame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PongGame extends JComponent {
 
@@ -10,6 +11,25 @@ public class PongGame extends JComponent {
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 600);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        // draw the sky
+        g.setColor(new Color(178, 223, 224));
+        g.fillRect(0, 0, 800, 600);
+
+        // draw the paddle
+        g.setColor(new Color(110, 61, 23));
+        g.fillRect(0, 510, 150, 15);
+
+        // draw the ball
+        g.setColor(new Color(155, 93, 169));
+        g.fillOval(400, 150, 30, 30);
     }
 
 }
